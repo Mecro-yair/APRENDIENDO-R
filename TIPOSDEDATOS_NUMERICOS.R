331722b9e8cc522f8361b22418aa30065a5601c0
Mecro-yair <dluquep@unjbg.edu.pe>
2024-12-03 11:46:34 -0500

    Agregar tipo de dato numerico

---
+++ TIPOSDEDATOS_NUMERICOS.R
@@ -0,0 +1,35 @@
+#NUMEROS POSITIVOS
+numero <- 10
+
+numero + 1
+numero -1
+
+numero * 2
+
+numero **2
+
+numero %%8
+
+
+#NUMEROS NEGATIVOS
+
+negativo <- -15
+
+negativo * -1
+
+negativo + 15
+
+negativo ** 2
+
+
+# DECIMALES
+
+decimal <-  2.71828
+ceiling(decimal) # entero mayor mas cercano
+floor(decimal) # entero menor mas cercano
+
+trunc(decimal) # Eliminacion de los decimales
+
+#convertir 2.71828 a 2.71
+
+trunc(decimal*100)/100
